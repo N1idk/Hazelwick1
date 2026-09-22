df27561c66c02919fbf82ab9d383c14a9c5f9023
N1idk <24naiduni@hazelwick.org>
2026-09-22 08:52:37 +0000

    22/09/26

---
+++ N1idk/task15.py
@@ -0,0 +1,4 @@
+l = int(input("Enter a length for your rectangle in cm : "))
+w = int(input("Now enter a width for your rectangle in cm : "))
+p = (l+w)*2
+print(f"The perimeter of your rectangle is {p}.")
