ce91641face2fbb9a9cd4250c70abf0139e320ed
N1idk <24naiduni@hazelwick.org>
2026-09-25 13:55:32 +0000

    25/09/26

--- N1idk/task22.py
+++ N1idk/task22.py
@@ -1,3 +1,3 @@
 age = int(input("Enter your age: "))
 if age >= 17: print("You are legally old enough to drive.")
-else : print("You are legally not old enough to drive.")
+else : print(f"You can drive in {17 -age} year")
